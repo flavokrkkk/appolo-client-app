8fc20df660167d8ebdc4fdfdf519b97a256593ae
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2025-01-28 21:05:48 +0300

    small fix

--- src/features/todo/hooks/useDeleteTodo.ts
+++ src/features/todo/hooks/useDeleteTodo.ts
@@ -1,12 +1,19 @@
 import { useMutation } from "@apollo/client";
-import {
-  deleteTodo,
-  getAllTodo,
-} from "../../../entities/todo/libs/todoApolloService";
+import { deleteTodo } from "../../../entities/todo/libs/todoApolloService";
 
 export const useDeleteTodo = () => {
   const [mutation, { loading, error }] = useMutation(deleteTodo(), {
-    refetchQueries: [{ query: getAllTodo() }],
+    update: (caches, { data: { removeTask } }) => {
+      caches.modify({
+        fields: {
+          allTasks(currentTodos: readonly { __ref: string }[]) {
+            return currentTodos.filter(
+              (todo) => todo.__ref !== `Task:${removeTask.id}`
+            ) as { __ref: string }[];
+          },
+        },
+      });
+    },
   });
 
   const handleDeleteTodo = (event: React.MouseEvent<HTMLButtonElement>) => {
